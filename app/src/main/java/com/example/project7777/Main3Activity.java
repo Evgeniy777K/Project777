cad6ee570232f7c0ed2e209fd678e90f05f6cf99
UUCTR2334 <ol3qnikolaev@yandex.com>
2019-12-15 11:19:54 +0200

    Update Main3Activity.java

--- app/src/main/java/com/example/project7777/Main3Activity.java
+++ app/src/main/java/com/example/project7777/Main3Activity.java
@@ -19,7 +19,7 @@ public class Main3Activity extends AppCompatActivity {
 
     String shoresh;
     String time;
-    List<VerbRow> verbs;
+    ArrayList<VerbRow> verbs = MainActivity.verbs;
     long counter = 0;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -30,7 +30,6 @@ public class Main3Activity extends AppCompatActivity {
 
         shoresh = getIntent().getStringExtra("shoresh");
         time = getIntent().getStringExtra("time");
-        verbs = MainActivity.verbs;
 
 
         List<String> results = new ArrayList<>();
@@ -56,7 +55,7 @@ public class Main3Activity extends AppCompatActivity {
         listView.setAdapter(adapter);
 
         Log.d("qaz", String.valueOf(results));
-        
+
         String textToBeTranslated = "אוֹהֵב";
         String languagePair = "he-ru";
         Translate(textToBeTranslated, languagePair);
